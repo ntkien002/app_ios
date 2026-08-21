05d910867d8ecee220fdb206198f04cc826d1e8a
ntkien002 <162787207+ntkien002@users.noreply.github.com>
2026-08-21 17:51:57 +0700

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.vocabtrainer',
+  appName: 'Vocab Trainer',
+  webDir: 'dist'
+};
+
+export default config;
